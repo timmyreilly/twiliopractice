8f80c52316794f2b7ea296c6d669a768cb6c97df
Tim Reilly <timreilly@live.com>
2015-05-19 13:31:28 -0700

    added first twilio call

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 #Main file
-import tokens.py
+from tokens import *
+
+from twilio.rest import TwilioRestClient
+
+ACCOUNT_SID = get_account_sid()
+AUTH_TOKEN = get_auth_token()
+TWILIO_NUMBER = get_twilio_number()
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+message = client.messages.create(
+    body="Hey Hey Hey",
+    to="+15033143771",    # Replace with your phone number
+    from_=TWILIO_NUMBER) # Replace with your Twilio number
+print message.sid
+
+
 
