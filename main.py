e84918479e6f89b20bffa34f63cd56bf6ba8776f
Tim Reilly <timreilly@live.com>
2015-05-19 13:35:04 -0700

    added abstraction of my phone number

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from twilio.rest import TwilioRestClient
 ACCOUNT_SID = get_account_sid()
 AUTH_TOKEN = get_auth_token()
 TWILIO_NUMBER = get_twilio_number()
+MY_PHONE_NUMBER = get_phone_number()
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 message = client.messages.create(
     body="Hey Hey Hey",
-    to="+15033143771",    # Replace with your phone number
+    to=MY_PHONE_NUMBER,    # Replace with your phone number
     from_=TWILIO_NUMBER) # Replace with your Twilio number
 print message.sid
 
