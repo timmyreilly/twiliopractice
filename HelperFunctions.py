f81a608b9d8dfdc36190d59f526c2d828597828e
Tim Reilly <timreilly@live.com>
2015-05-20 10:50:51 -0700

    created helper function

---
+++ HelperFunctions.py
@@ -0,0 +1,22 @@
+from tokens import *
+
+def get_working_queue():
+    return WORKING_QUEUE
+
+def get_storage_account():
+    return STORAGE_ACCOUNT
+
+def get_storage_key():
+    return STORAGE_KEY
+
+def get_account_sid():
+	return ACCOUNT_SID
+	
+def get_auth_token():
+	return AUTH_TOKEN
+
+def get_twilio_number():
+    return TWILIO_SANDBOX_NUMBER
+
+def get_phone_number():
+    return MY_PHONE_NUMBER
